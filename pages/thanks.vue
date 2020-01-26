3fbf5c839ce2a3b580c1453ddf4264ad921f452d
captproton <carl@wdwhub.net>
2020-01-25 16:24:56 -0800

    CHANGE: modified thanks.vue style

--- pages/thanks.vue
+++ pages/thanks.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    <a href="/" class="thanks">Thanks. It's good to hear from you.</a>
+    <p class="thank-you">Thanks, it's good to hear from you.</p>
+    <p>
+      If you have time, please take a look at the
+      <nuxt-link to="/writings">articles</nuxt-link>&nbsp;or
+      <nuxt-link to="/projects">projects</nuxt-link>.
+    </p>
   </div>
 </template>
 
@@ -9,4 +14,13 @@ export default {};
 </script>
 
 <style lang="scss" scoped>
+p {
+  color: #b5b3b3;
+  font-size: 18px;
+}
+a {
+  color: white;
+}
+.thank-you {
+}
 </style>
